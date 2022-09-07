9bbfd4c864ff8acdb0afbc7415c1ed7ba502a742
Rahul Gupta <mail2guptar@gmail.com>
2022-09-07 09:11:49 +0530

    Added Tabline

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -36,6 +36,31 @@ require('packer').startup(function(use)
     use 'L3MON4D3/LuaSnip'
     use 'saadparwaiz1/cmp_luasnip'
     use 'ekickx/clipboard-image.nvim'
+    use { 'kdheepak/tabline.nvim', config = function()
+                                    require'tabline'.setup {
+                                        -- Defaults configuration options
+                                   enable = true,
+                                   options = {
+                                        -- If lualine is installed tabline will use separators configured in lualine by default.
+                                        -- These options can be used to override those settings.
+                                        section_separators = {'', ''},
+                                        component_separators = {'', ''},
+                                        max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+                                        show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
+                                        show_devicons = true, -- this shows devicons in buffer section
+                                        show_bufnr = false, -- this appends [bufnr] to buffer section,
+                                        show_filename_only = false, -- shows base filename only instead of relative path in filename
+                                        modified_icon = "+ ", -- change the default modified icon
+                                        modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
+                                        show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+                                      }
+                                    }
+        vim.cmd[[
+                    set guioptions-=e " Use showtabline in gui vim
+                    set sessionoptions+=tabpages,globals " store tabpages and globals in session
+               ]]
+        end,
+   }
     use { 'vimwiki/vimwiki', branch = 'master' }
 -- use 'marko-cerovac/material.nvim'
   end
@@ -175,6 +200,7 @@ require('formatter').setup({
     	}
     }
 })
+vim.cmd([[let g:shfmt_opt="-ci"]])
 
 -- neovim/nvim-lspconfig
 local nvim_lsp = require'lspconfig'
