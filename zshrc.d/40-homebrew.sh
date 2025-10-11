346b6b4a08df5e41a5b40200cda0ec1d7908036a
Rahul Gupta <mail2guptar@gmail.com>
2025-10-11 15:43:51 +0530

    🔧 Add automated setup scripts (brew, symlink, postinstall, backup system)

--- zshrc.d/40-homebrew.sh
+++
@@ -1,2 +0,0 @@
-[ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
-
--- zshrc.d/54-browser.sh
+++
@@ -1,6 +0,0 @@
-kite () { open -a Brave\ Browser "https://kite.zerodha.com/" } 
-hnews () { open -a Brave\ Browser "https://news.ycombinator.com/" } 
-twitter () { open -a Brave\ Browser "https://twitter.com/" } 
-speedtest () { open -a Brave\ Browser "https://www.speedtest.net" } 
-whatsapp () { open -a Brave\ Browser "https://web.whatsapp.com" } 
-
